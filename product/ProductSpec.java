b699b830edc0f1158da8a1e43affb5afde8c1265
Ye Cheng <ycheng82@gmail.com>
2014-02-07 14:07:57 -0800

    Test

---
+++ product/ProductSpec.java
@@ -0,0 +1,27 @@
+package product;
+
+public class ProductSpec {
+	private Integer upc;	
+	private String description;	
+	private float price;
+
+	public ProductSpec(String[] productInfor) {
+		// TODO Auto-generated constructor stub
+		//System.out.println("get infor");
+		this.upc = Integer.valueOf(productInfor[0]);
+		this.description = productInfor[1];
+		this.price = Float.valueOf(productInfor[2]);
+	}
+
+	public Integer getUpc() {
+		return this.upc;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+	public float getPrice(){
+		return this.price;
+	}
+
+}
